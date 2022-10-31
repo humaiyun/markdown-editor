0ec22805b7bf5ac432519db39ea7ea0bd8e13b68
Humaiyun Uddin <uddin513@msn.com>
2022-10-31 13:00:35 -0400

    Structured the main app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React, { useState } from 'react';
 import './App.css';
+import { MarkdownInput } from './components/markdown/input/MarkdownInput.jsx';
+import { MarkdownOutput } from './components/markdown/output/MarkdownOutput.jsx';
+import EditorContext from './components/context/EditorContext.js';
+import { AppContainer, Title, EditorContainer } from "./styles.js";
 
 const App = () => {
+    const [markdownText, setMarkdownText] = useState("");
+    const contextValue = {
+        markdownText,
+        setMarkdownText
+    };
+
     return (
-        <div className='App'>
-            <h1>Home</h1>
-        </div>
+        <EditorContext.Provider value={contextValue}>
+            <AppContainer>
+                <Title>Markdown Editor</Title>
+                <EditorContainer>
+                    <MarkdownInput />
+                    <MarkdownOutput />
+                </EditorContainer>
+            </AppContainer>
+        </EditorContext.Provider>
     );
 }
 
